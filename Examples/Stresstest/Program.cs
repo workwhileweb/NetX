1ef1e7af3e6919c25ca633502cfed03aaed01253
Grand Silence <mixtape774@gmail.com>
2019-03-28 07:45:13 +0300

    Added HttpProxyClient.DebugProxy and Socks5ProxyClient.DebugProxy for Charles & Fiddler support. Added Example Cloudflare bypass project.

--- Examples/Stresstest/Program.cs
+++ Examples/Stresstest/Program.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Threading.Tasks;
 
-namespace Leaf.xNet.Stresstest
+namespace Stresstest
 {
     internal class Program
     {
--- Examples/Stresstest/Properties/AssemblyInfo.cs
+++ Examples/Stresstest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Leaf.xNet.Stresstest")]
+[assembly: AssemblyTitle("Stresstest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Leaf.xNet.Stresstest")]
+[assembly: AssemblyProduct("Stresstest")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
